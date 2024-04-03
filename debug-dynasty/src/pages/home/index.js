7e5167954bead6cc75ef0b2cbae706e04a492c31
Julie Maricic-Detweiler <j.maricicdetweil@ufl.edu>
2024-04-03 18:11:10 -0400

    created react app and modified existing files

---
+++ debug-dynasty/src/pages/home/index.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1 style={{textAlign: "center", fontFamily: "sans-serif"}}>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
