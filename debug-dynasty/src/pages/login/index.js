a0b7cbfe4bf070be82458d9964e48c7d3c94fe97
marquitos150 <mmvazquez364@gmail.com>
2024-04-09 21:50:53 -0400

    Created basic Login UI for the application.

--- debug-dynasty/src/pages/login/index.js
+++ debug-dynasty/src/pages/login/index.js
@@ -1,7 +1,33 @@
+import React from 'react';
+import './index.css';
+
 const Login = () => {
     return (
         <div className="login-container">
-            <h1 style={{textAlign: "center"}}>Login</h1>
+            <form action="">
+                <h1>Login</h1>
+                <h3>Enter your email and password to log in!</h3>
+                <hr></hr>
+                <div className="input-box">
+                    <h3>Email*</h3>
+                    <input type="text" placeholder="debugger@whatever.com" required />
+                </div>
+                <div className="input-box">
+                    <h3>Password*</h3>
+                    <input type="password" placeholder="Min. 8 characters" required />
+                </div>
+
+                <div className="keep-forgot">
+                    <label><input type="checkbox" />Keep me logged in</label>
+                    <a href="#">Forgot password?</a>
+                </div>
+
+                <button type="submit">Login</button>
+
+                <div className="register-link">
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+            </form>
         </div>
     );
 }
