91e0467d25e4e45abfee25a622c91920cf61b7c4
marquitos150 <mmvazquez364@gmail.com>
2024-04-10 12:26:53 -0400

    Created additional files and added logo for app.

--- debug-dynasty/src/pages/home/index.js
+++ debug-dynasty/src/pages/home/index.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div className="home-container">
             <h1 style={{textAlign: "center", fontFamily: "sans-serif"}}>Home</h1>
+            <img src="/logo-final.png" alt="" />
         </div>
     )
 }
---
+++ debug-dynasty/src/pages/navbar/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './index.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    
+                </Link>
+            </div>
+        </nav>
+    )
+}
