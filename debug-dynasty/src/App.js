a5a637b2f739b87c2a091206fc04922ec1a7eaaf
Julie Maricic-Detweiler <j.maricicdetweil@ufl.edu>
2024-05-02 02:09:52 -0400

    added project page

--- debug-dynasty/src/App.js
+++ debug-dynasty/src/App.js
@@ -9,6 +9,7 @@ import Signup from './pages/signup';
 import Documentation from './pages/documentation';
 import Register from './pages/registerProject';
 import AddSelf from './pages/addToProject';
+import ProjectDashboard from './pages/project';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<Signup/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/project" element={<ProjectDashboard/>} />
         <Route path="/docs" element={<Documentation />} />
         <Route path="/register" element={<Register/>}/>
         <Route path="/addMe" element={<AddSelf/>} />
